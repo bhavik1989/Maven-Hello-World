7f6749f718ba9f5f6eeea50bde4c10b91a53516c
Samp <patelbhavik89@gmail.com>
2018-01-02 20:38:57 -0500

    added a new project

---
+++ src/main/java/com/example/bah/helloworld/App.java
@@ -0,0 +1,18 @@
+package com.example.bah.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello BAH!");
+    }
+    public void main1(  )
+    {
+        System.out.println("Hello BAH!");
+    }
+}
